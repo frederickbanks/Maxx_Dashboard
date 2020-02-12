0b54c178b9e49f65cb86936dac21400220635f9a
frederick <frederickbanks@icloud.com>
2020-02-12 01:58:55 -0500

    added files for components // getting error regarding scope/import or export of first component...may change to a class based component to fix issue for MVP

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import ReactDom from 'react-dom'
+
+export default Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <ul>
+        <li>Dashboard</li>
+        <li>Widget</li>
+        <li>Reviews</li>
+        <li>Customers</li>
+        <li>Online Analysis</li>
+        <li>Settings</li>
+      </ul>
+    </div>
+  );
+};
+
+// export default Sidebar
